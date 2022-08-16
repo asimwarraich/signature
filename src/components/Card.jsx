8738cc0a22b6beb07df4c250decbac65b8708037
asimwarraich <asimwarraich533@gmail.com>
2022-08-16 01:53:14 -0700

    changings

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import nature from "../assets/nature.png";
+import signature from "../assets/signature.png";
+
+function Card() {
+  return (
+    <div>
+      <div className="nature__container">
+        <div className="nature__logo__start">
+          <img src={signature} alt="signature" />
+        </div>
+        <div className="nature__container__left">
+          <div className="nature__container__left__text">
+            <div className="nature__container__left__text__heading">
+              Closer to Nature
+            </div>
+            <div className="nature__container__left__text__sub__heading">
+              While easing you into a calmer pace of life, Signature Hotels
+              Murree's all-valley facing lavish suites blend modern silhouettes
+              with contemporary space to create inviting retreats.
+            </div>
+          </div>
+        </div>
+        <div className="nature__container__right">
+          <div className="nature__container__right__img">
+            <img src={nature} alt="nature" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
