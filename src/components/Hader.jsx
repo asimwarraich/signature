6a9ea4db4628f556ae66984e90fd1a03e77786ed
asimwarraich <asimwarraich533@gmail.com>
2022-08-17 06:42:32 -0700

    nav__bar__responsive__done

--- src/components/Hader.jsx
+++ src/components/Hader.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-
-function Hader() {
+import React, { useLayoutEffect } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
+export default function Hader() {
+  const [isOpen, setIsOpen] = React.useState(true);
+  const [isScrolling, setIsScrolling] = React.useState(false);
+  function checkNavOpenClose() {
+    if (window.innerWidth < 900) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  }
+  useLayoutEffect(() => {
+    checkNavOpenClose();
+    window.addEventListener("resize", checkNavOpenClose);
+    window.addEventListener("scroll", () => {
+      checkNavOpenClose();
+      if (window.scrollY > 0) {
+        setIsScrolling(true);
+      } else {
+        setIsOpen(false);
+      }
+    });
+  }, []);
   return (
     <div>
-      {" "}
       <div className="sign__nav__bar__wraper">
         <div className="signature__header__logo">
           <button>
@@ -214,27 +234,120 @@ function Hader() {
             </svg>
           </button>
         </div>
-        <div className="sign__nav__links__wraper">
-          <a href="#" className="sign__nav__link">
-            Home
-            <title>Home</title>
-          </a>
-          <a href="#" className="sign__nav__link">
-            About us
-          </a>
-          <a href="#" className="sign__nav__link">
-            Amenities
-          </a>
-          <a href="#" className="sign__nav__link">
-            Location
-          </a>
-          <a href="#" className="sign__nav__link">
-            Contact Us
-          </a>
-        </div>
+        <button
+          className="signature__contant__nav__menu"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+          title="menu"
+        >
+          {isOpen ? (
+            <Close size="24" color="white" />
+          ) : (
+            <Menu size="24" color="white" />
+          )}
+        </button>
+        {isOpen ? (
+          <div className="sign__nav__links__wraper">
+            <a
+              href="#"
+              onClick={() => {
+                if (window.innerWidth < 900) {
+                  setIsOpen(false);
+                }
+              }}
+              className="sign__nav__link"
+            >
+              Home
+              <title>Home</title>
+            </a>
+            <a
+              href="#"
+              onClick={() => {
+                if (window.innerWidth < 900) {
+                  setIsOpen(false);
+                }
+              }}
+              className="sign__nav__link"
+            >
+              About us
+            </a>
+            <a
+              href="#"
+              onClick={() => {
+                if (window.innerWidth < 900) {
+                  setIsOpen(false);
+                }
+              }}
+              className="sign__nav__link"
+            >
+              Amenities
+            </a>
+            <a
+              href="#"
+              onClick={() => {
+                if (window.innerWidth < 900) {
+                  setIsOpen(false);
+                }
+              }}
+              className="sign__nav__link"
+            >
+              Location
+            </a>
+            <a
+              href="#"
+              onClick={() => {
+                if (window.innerWidth < 900) {
+                  setIsOpen(false);
+                }
+              }}
+              className="sign__nav__link"
+            >
+              Contact Us
+            </a>
+          </div>
+        ) : null}
       </div>
     </div>
   );
 }
 
-export default Hader;
+function Close({ size, color }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-x"
+    >
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+  );
+}
+function Menu({ size, color }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-menu"
+    >
+      <line x1="3" y1="12" x2="21" y2="12"></line>
+      <line x1="3" y1="6" x2="21" y2="6"></line>
+      <line x1="3" y1="18" x2="21" y2="18"></line>
+    </svg>
+  );
+}
